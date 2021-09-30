1f6ffd08f782d27460f796d9c485f0f2462c71de
Alanna Soepler <N00192732@iadt.ie>
2021-09-30 11:47:39 +0100

    I have added another welcome message

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello There <br>";
+echo "Hello There Again <br>";
+echo "I am in space"
+?>
+ 
+    
+    
+<a href="myFirstPHP.php">Click here to go to another page</a>
+
+</body>
+</html>
